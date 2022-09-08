644a2a63d6adff2ffe9eb7a2d7f57521b90c3a6a
황규범 <bany981111@gmail.com>
2022-09-09 08:54:14 +0900

    Add files via upload

---
+++ 05_if_for_while/01_if_abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = -150;
+    if (x < 0)
+    {
+        x = -x;
+    }
+    else
+    {
+        x = x;
+    }
+    printf("x의 절댓값은 %d입니다. \n", x);
+    return 0;
+}
